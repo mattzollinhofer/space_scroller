79a3389d2507e02e973dc8b07e46728ebdda9ac9
mattzollinhofer <matt.zollinhofer@gmail.com>
2026-07-26 04:37:12 +0000

    Deploying to gh-pages from @ mattzollinhofer/space_scroller@2fb38ff455004bc83964a1ed5311a85877566551 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785039314|6360621';
+const CACHE_VERSION = '1785040615|6535691';
 /** @type {string} */
 const CACHE_PREFIX = 'Solar System Sho-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
